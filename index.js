6c032a0af854ca44373319908865f55b69593884
MsWakesho <wakeshosonja@gmail.com>
2023-12-14 17:25:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
-// Write your code here
+function multiply(num1, num2) {
+  return num1  *  num2;              
+}
+
+console.log(multiply( 31 , 2))
+
+
+
+function random() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+ 
+
+
+function mod(num3, num4) {
+  return num3 % num4;
+}
+
+let result = mod(14, 5)
+console.log(result)
+
+
+
+
+function max(numbers) {
+  return Math.max(...numbers);
+}
+
+
+
+
+
+  
+  
